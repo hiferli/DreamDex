4ed372deff6afec68dffe8d1b56b80d001af8525
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-31 23:17:34 +0530

    Importing and Exporting Components

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { useState , useEffect } from "react"
+import React  from 'react'
+
+import {Loader , FormField , Card} from '../Components'
 
 const Home = () => {
   return (
