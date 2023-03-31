70702b5788f7602bbc85ddc06cc7a0e00b402f75
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-31 23:00:26 +0530

    Added Create Button

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,11 @@ const App = () => {
 				<Link to="/">
 					<img src={logo} alt="Logo" className='w-28 object-contain'/>
 				</Link>
+
+				<Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
 			</header>
+
+			
 		</BrowserRouter>
 	)
 }
