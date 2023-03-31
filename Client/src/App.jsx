c6c4bcc208ef0e79acdabfdedce401717ac45b88
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-31 22:58:30 +0530

    Creating Header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { BrowserRouter , Link , Route , Routes } from 'react-router-dom'
-import {logo} from './Assets';
-import { Home , CreatePost } from './Pages'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { logo } from './Assets';
+import { Home, CreatePost } from './Pages'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+				<Link to="/">
+					<img src={logo} alt="Logo" className='w-28 object-contain'/>
+				</Link>
+			</header>
+		</BrowserRouter>
+	)
 }
 
 export default App
