af7235208030610f0f7458371aa75ee71979c8b1
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-31 21:53:46 +0530

    Importing Files to App.js

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter , Link , Route , Routes } from 'react-router-dom'
 import {logo} from './Assets';
+import { Home , CreatePost } from './Pages'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    </BrowserRouter>
   )
 }
 
