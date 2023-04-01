6caf185d6eb66482cdc5d75576ccb1fb9c8c2594
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-01 16:15:06 +0530

    Creating Express App

---
+++ Server/Index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/' , async (request , response , next) => {
+    response.send("Hello from Your World!")
+})
+
+const startServer = async () => {
+    app.listen(3000 , () => {
+        console.log("Listening on PORT: 3000")
+    })
+}
+
+startServer();
