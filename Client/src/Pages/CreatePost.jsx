b504ae608724bd31516f8dbfac5513c17e72be7e
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-01 11:22:31 +0530

    Adding Files to Create Post Page

--- Client/src/Pages/CreatePost.jsx
+++ Client/src/Pages/CreatePost.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+import { preview } from '../Assets'
+import { getRandomPrompt } from '../Utils'
+import { FormField , Loader } from '../Components' 
 
 const CreatePost = () => {
+  
   return (
     <div>
       Create Post
