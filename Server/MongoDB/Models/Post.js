8122d8c919ff21a54a277517b3f3b48d31efc349
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-01 17:42:07 +0530

    Creating MongoDB Schema

---
+++ Server/MongoDB/Models/Post.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const post = new mongoose.Schema({
+    name : {
+        type: String,
+        require: true
+    } ,
+    
+    prompt : {
+        type: String,
+        require: true
+    } ,
+    
+    photo : {
+        type: String,
+        require: true
+    } ,
+})
+
+// Creates the model out of the schema
+const PostSchema = mongoose.model("Post" , post)\
+export default PostSchema
