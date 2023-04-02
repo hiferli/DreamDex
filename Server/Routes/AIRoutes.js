17ed5f65ad1f30aced566aa99e446f083f973649
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-02 09:14:22 +0530

    Getting Image from AI API

--- Server/Routes/AIRoutes.js
+++ Server/Routes/AIRoutes.js
@@ -1,10 +1,54 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import  { Configuration , OpenAIApi } from 'cloudinary'
-
-import Post from '../MongoDB/Models/Post.js'
+import express, { request, response } from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 // Creating Routes
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openAI = new OpenAIApi(configuration);
+
+
+// router.route("/").get((request , response , next) => {
+//     // response.send("")
+//     response.status(200).json({ "text" : "Hello From OpenAI"});
+// })
+
+router.route("/").post(async (request , response , next) => {
+    try {
+        const { prompt } = request.body;
+        console.log(prompt)
+        console.log("Line 25")
+        
+        // const AIResponse = await openAI.createImage({
+        //     prompt,
+        //     n: 1,
+        //     size: '1024x1024',
+        //     response_format: 'b64_json',
+        // })
+
+        // NOT WORKING... Since the API is a Paid one and I don't have that much to pay to the same
+        const response = await openAI.createImage({
+            prompt: "a white siamese cat",
+            n: 1,
+            size: "1024x1024",
+          });
+          image_url = response.data.data[0].url;
+          
+
+        console.log("Line 32")
+
+        // const image = AIResponse.data.data[0].b64_json;
+        response.status(200).json({ photo: image_url });
+
+    } catch (error) {
+        console.log("Error in Fetching Data from AI: " + error);
+        response.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
+})
+
+export default router;
