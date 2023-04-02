f623380cfb371cb38f2b15901ed274f6aea98739
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-02 13:34:03 +0530

    Loading Data from Database on Home Screen

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 import { Loader, FormField, Card } from '../Components'
 
-const RenderCards = ({data , title}) => {
-    if(data?.length > 0){
+const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
         return data.map((post) => {
-            <Card key={post._id} {...post}/>
+            <Card key={post._id} {...post} />
         })
     }
 
@@ -17,6 +17,33 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch("http://localhost:3000/api/v1/post", {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+
+                if (response.ok) {
+                    const result = response.json();
+                    setAllPosts(result.data.reverse());
+                }
+            } catch (error) {
+                console.log('Error while fetching all posts from the Database: ' + error);
+                alert(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchPosts();
+    }, []);
 
     return (
         <section className="max-w-7xl mx-auto ">
@@ -37,15 +64,15 @@ const Home = () => {
                 ) : (
                     <>
                         {searchText && (
-                             <h2 className="font-medium text-[#666e75] text-xl mb-3">Showing Results for <span className="text-[#222328]">{searchText}</span> </h2>
+                            <h2 className="font-medium text-[#666e75] text-xl mb-3">Showing Results for <span className="text-[#222328]">{searchText}</span> </h2>
                         )}
 
                         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-                            {searchText ? 
+                            {searchText ?
                                 // Would contain actual data 
-                                <RenderCards data={[]} title='No Search Results Found!'/>
+                                <RenderCards data={[]} title='No Search Results Found!' />
                                 :
-                                <RenderCards data={[]} title='No Posts Found!'/>
+                                <RenderCards data={allPosts} title='No Posts Found!' />
                             }
                         </div>
                     </>
