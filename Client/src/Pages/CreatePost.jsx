60c8f45e6427e5e7742e48c1ff7bbfd5f4b0c614
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-02 13:21:41 +0530

    Submitting Created Image to Backend

--- Client/src/Pages/CreatePost.jsx
+++ Client/src/Pages/CreatePost.jsx
@@ -16,8 +16,32 @@ const CreatePost = () => {
 	const [generatingImage, setGeneratingImage] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+
+		if(form.prompt && form.photo){
+			setLoading(true);
+
+			try {
+				const response = await fetch("http://localhost:3000/api/v1/post" , {
+					method: "POST",
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(form)
+				})
+
+				await response.json();
+				navigate("/")
+			} catch (error) {
+				console.log("Error in Submitting the Form: " + error);
+				alert(error);
+			} finally {
+				setLoading(false)
+			}
+		} else {
+			alert("Please enter the Prompt and Generate the Image")
+		}
 	}
 
 	const handleChange = () => {
