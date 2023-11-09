10945e9602a40db4dc7f664e37a2072803960ee9
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-11-09 19:59:35 +0530

    Fixing PayloadExceedLimit

--- Server/Index.js
+++ Server/Index.js
@@ -11,7 +11,8 @@ dotenv.config()
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 app.use("/api/v1/post" , postRoutes);
 app.use("/api/v1/ai" , AIRoutes);
